e2b23dffa8c5cb91afd781c97dbae3b9fc47a7e5
hashcowuwu <3189506476@qq.com>
2024-08-21 12:17:52 +0800

    add assignment 3

--- assignment/weatherforecast_starter/main.cpp
+++ assignment/weatherforecast_starter/main.cpp
@@ -8,24 +8,32 @@ Submit to Paperless by 11:59pm on 5/10/2024.
 */
 
 // TODO: import anything from the STL that might be useful!
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 
 // TODO: write convert_f_to_c function here. Remember it must be a template function that always returns
 // a double.
 // [function definition here]
-
+template<typename  T> 
+double convert_f_to_c(T f) {
+  return (f - 32) * 5.0 * 9.0;
+}
 template<typename Function>
 std::vector<double> get_forecast(std::vector<std::vector<double>> readings, Function fn) {
     // TODO: write get_forecast here!
 
-  std::vector<double>res;
-  for(int i = 0 ; i < readings.size() ; ++ i ) {
-    for (int j = 0 ; j < readings[i].size() ; ++ i ) {
-
+  std::vector<double>result;
+  for (const auto& day : readings) {
+    for (const auto& temp : day) {
+      result.push_back(fn(temp));
     }
   }
+
+  return result;
 
 }
 
@@ -41,12 +49,35 @@ int main() {
     };
 
     // TODO: Convert temperatures to Celsius and output to output.txt
+  std::ofstream output("output.txt");
+  auto celsius_readings = get_forecast(readings, convert_f_to_c<double>);
+  for (const auto& temp:celsius_readings) {
+    output << temp << ' ';
+  }
+  output << std::endl;
 
     // TODO: Find the maximum temperature for each day and output to output.txt
 
+  for (const auto &day :readings) {
+    auto maxTemp = *std::max_element(day.begin(),day.end());
+    output << convert_f_to_c(maxTemp) << ' ';
+  }
+  
+  output << std::endl;
     // TODO: Find the minimum temperature for each day and output to output.txt
 
+  for (const auto &day :readings) {
+    auto minTemp = *std::min_element(day.begin(),day.end());
+    output << convert_f_to_c(minTemp) << ' ';
+  }
     // TODO: Find the average temperature for each day and output to output.txt
+  output << std::endl;
 
-    return 0;
+  for (const auto &day :readings) {
+    double sum = std::accumulate(day.begin(),day.end(), 0.0); 
+    double avg = sum / day.size();
+    output << convert_f_to_c(avg) << ' ';
+  }
+  output << std::endl;
+  return 0;
 }
